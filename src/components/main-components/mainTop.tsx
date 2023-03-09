5e7d8c519a0f2e596edc3ec8a88599482aacf7f6
Barel_Uklis <barel.uklis@gmail.com>
2023-03-09 17:38:58 +0200

    FIX - fixed some stuff on landing page

--- src/components/main-components/mainTop.tsx
+++ src/components/main-components/mainTop.tsx
@@ -5,22 +5,32 @@ import TextSlideHook from "@/hooks/textSlideHook"
 import homePageDictionary from "@/lang/main-page.json"
 import rootStore from "@/store"
 import { observer } from "mobx-react-lite"
+import Image from "next/image"
+import logoDark from '../../../public/Develobar-Logo-Dark.png'
+import logoLight from '../../../public/Develobar-Logo-Light.png'
+import { useTheme } from "next-themes"
 
 const MainTop = observer(() => {
   const { uiStore } = rootStore;
+  const { theme } = useTheme();
   const appDirection = getAppDirection();
-  TextSlideHook('sliding-sub-header');
+  //TextSlideHook('sliding-sub-header');
   return (
     <div  className={`${styles.mainTop} ${styles.mainScreen} ${styles.mainAnimation}`}>
     <Row className={styles.mainTopTRow}>
-      <Typography.Title id='header'>{homePageDictionary.header[uiStore.pageLanguage]}</Typography.Title>
+      <Image
+        src={theme === 'dark' ? logoLight : logoDark}
+        alt="Develobar Logo"
+        className={styles.mainTopLogo}
+      />
     </Row>
     <Row className={styles.mainTopBRow}>
-      <Typography.Title id='sliding-sub-header' level={2}>{
-        homePageDictionary.subHeader[uiStore.pageLanguage].split(',').map((item, index) => {
+      <Typography.Title level={1} className={styles.header} id='header'>{homePageDictionary.header[uiStore.pageLanguage]}</Typography.Title>
+      <Typography.Title level={2} id='sliding-sub-header' className={styles.slidingSubHeader}>{
+        homePageDictionary.subHeader[uiStore.pageLanguage].split(',,').map((item, index) => {
           return <span key={index} style={{
             marginLeft: appDirection === "ltr" ? `${index * 5}rem` : 0,
-            marginRight: appDirection === "rtl" ? `${index * 9}rem` : 0,
+            marginRight: appDirection === "rtl" ? `${index * 2}rem` : 0,
             lineHeight: '1.5',
           }}>{item}<br /></span>
         })
